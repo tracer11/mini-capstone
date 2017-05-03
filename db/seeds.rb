3dc9baa40141f03b61fe5307bdf91564333e96e2
tracer11 <karolgurba22@gmail.com>
2017-05-02 23:15:18 -0500

    added 2 new cars

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,17 @@
-item = Item.new(name: "Nissan GTR", color: "blue", price: 1500, image: "https://i.ytimg.com/vi/LU6y4F4bD8Q/maxresdefault.jpg", description: "JDM Legend")
+#item = Item.new(name: "Nissan GTR", color: "blue", price: 1500, image: "https://i.ytimg.com/vi/LU6y4F4bD8Q/maxresdefault.jpg", description: "JDM Legend")
+#item.save
+
+#item = Item.new(name: "Toyota Supra", color: "white", price: 1400, image: "http://www.the-lowdown.com/media/mhemming/supra/post/Supra%202.jpg", description: "JDM Legend")
+#item.save
+
+#item = Item.new(name: "Subaru WRX STI", color: "blue", price: 1000, image: "https://www.revvolution.com/image/albums-4-59b-d6-d2-0/2005-subaru-impreza-wrx-sti-betsy-blue-32540.jpg", description: "Rally Legend")
+#item.save
+
+#item = Item.new(name: "Mitsubishi Evo", color: "silver", price: 1000, image: "http://ridesandstyling.com/database/images/2012/07/mitsubishi-evolution-ix-silver-white-42.jpg", description: "Rally Legend")
+#item.save
+
+item = Item.new(name: "Nissan Silvia", color: "White", price: 950, image: "http://ridesandstyling.com/database/images/2012/09/nissan-s15-silvia-white-bronze1.jpg", description: "Drift Missle")
 item.save
 
-item = Item.new(name: "Toyota Supra", color: "white", price: 1400, image: "http://www.the-lowdown.com/media/mhemming/supra/post/Supra%202.jpg", description: "JDM Legend")
-item.save
-
-item = Item.new(name: "Subaru WRX STI", color: "blue", price: 1000, image: "https://www.revvolution.com/image/albums-4-59b-d6-d2-0/2005-subaru-impreza-wrx-sti-betsy-blue-32540.jpg", description: "Rally Legend")
-item.save
-
-item = Item.new(name: "Mitsubishi Evo", color: "silver", price: 1000, image: "http://ridesandstyling.com/database/images/2012/07/mitsubishi-evolution-ix-silver-white-42.jpg", description: "Rally Legend")
+item = Item.new(name: "Nissan 370z", color: "Black", price: 850, image: "https://s-media-cache-ak0.pinimg.com/originals/96/d8/c6/96d8c671c6ffeadad37ae7cde9fafe62.jpg", description: "Great Daily Car")
 item.save
