0e148ea47027e7ad7656dc838b49798d2b7e3a07
tracer11 <karolgurba22@gmail.com>
2017-06-29 20:36:49 -0500

    updated url path for background image

--- db/migrate/20170516233222_rename_image_column.rb
+++ db/migrate/20170516233222_rename_image_column.rb
@@ -1,4 +1,4 @@
-class RenameImageColumn < ActiveRecord::Migration[5.0]
+ class RenameImageColumn < ActiveRecord::Migration[5.0]
   def change
     rename_column :images, :product_id, :item_id
   end
